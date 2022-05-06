b3001c031f1e30d0fec0723572b706fc09b20beb
amolikvivian <amolikvivian@gmail.com>
2022-05-07 03:51:50 +0530

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-between items-center w-full">
+    <div class="flex items-baseline">
+      <span class="md:text-4xl text-2xl"><b>Virtual cards</b></span>
+      <button
+        class="bg-gray-100 rounded-sm text-blue-600 border-none px-1 py-1 text-xs ml-4 flex items-center"
+      >
+        <Icon name="vid-rec" />
+        <span class="pl-1">Learn More</span>
+      </button>
+    </div>
+    <button
+      class="border shadow-md rounded-md px-3 py-1 text-sm flex items-center justify-between"
+    >
+      <Icon name="plus"/>
+      <span class="pl-2">Virtual Card</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import Icon from "./Icon";
+export default {
+  components: { Icon },
+};
+</script>
+
+<style></style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg
+    v-if="name === 'plus'"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-4 w-4"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="1.5"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+    />
+  </svg>
+  <svg
+    v-if="name === 'vid-rec'"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-4 w-4"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="1.5"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style></style>
