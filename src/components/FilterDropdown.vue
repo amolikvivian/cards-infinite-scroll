88c5ed6e6e1434fb3c28fa52ad8e5d1720325f3d
amolikvivian <amolikvivian@gmail.com>
2022-05-07 06:25:44 +0530

    Added: UI for filter component

---
+++ src/components/FilterDropdown.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <span class="tracking-wide"><b>Filters</b></span>
+    <hr class="h-0.5 bg-gray-200 my-1" />
+    <div class="px-2 py-4">
+      <div class="flex flex-col">
+        <span class="text-sm text-gray-500"><b>Type</b></span>
+        <div class="flex items-center justify-between">
+          <button
+            class="flex items-center text-sm mt-3"
+            v-for="(type, i) in cardTypes"
+            :key="i"
+            @click="selectType(type.val)"
+          >
+            <div
+              class="w-4 h-4 border border-gray-500 mr-2"
+              :class="selectedType === type.val ? 'bg-green-400' : ''"
+            ></div>
+            {{ type.name }}
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-col mt-8">
+        <span class="text-sm text-gray-500"><b>Cardholder</b></span>
+        <select
+          class="form-select px-1 py-1.5 text-sm font-normal mt-3 text-gray-700 bg-white border border-solid border-gray-300 rounded focus:border-blue-600 focus:outline-none"
+        >
+          <option selected disabled>Select Cardholder</option>
+          <option value="1">One</option>
+        </select>
+      </div>
+      <div class="flex items-center justify-between mt-12">
+        <button
+          @click="apply()"
+          class="text-sm px-4 py-1 bg-red-500 text-white w-28 rounded-md shadow"
+        >
+          Apply
+        </button>
+        <button
+          @click="clear()"
+          class="text-sm px-4 py-1 bg-white text-gray-700 w-28 rounded-md shadow"
+        >
+          <b>Clear</b>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardTypes: Array,
+  },
+  data() {
+    return {
+      selectedType: null,
+    };
+  },  
+  methods: {
+    selectType(val) {
+      this.selectedType = val;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="flex justify-end items-center">
-    <button class="pr-4"><Icon name="search" /></button>
-    <button class="px-3 py-2 bg-gray-100 text-gray-700 flex items-center text-sm rounded-md">
+    <!-- <button class="pr-4"><Icon name="search" /></button> -->
+    <button
+      class="px-3 py-2 bg-gray-100 text-gray-700 flex items-center text-sm rounded-md"
+      @click="toggleFilterMenu()"
+    >
       <Icon name="filter" />
       <span class="pl-1">Filter</span>
     </button>
+    <div
+      v-if="showFilterMenu"
+      class="z-999 bg-white border rounded shadow-lg h-80 w-80 absolute mt-96 mr-12 px-4 py-4"
+    >
+      <FilterDropdown :cardTypes="cardTypes" />
+    </div>
   </div>
 </template>
 
 <script>
 import Icon from "./Icon";
+import FilterDropdown from "./FilterDropdown";
 export default {
-  components: { Icon },
+  components: { Icon, FilterDropdown },
+  data() {
+    return {
+      showFilterMenu: false,
+      cardTypes: [
+        { name: "Burner", val: "burner" },
+        { name: "Subscription", val: "subscription" },
+      ],
+    };
+  },
+  methods: {
+    toggleFilterMenu() {
+      this.showFilterMenu = !this.showFilterMenu;
+    },
+  },
 };
 </script>
 
