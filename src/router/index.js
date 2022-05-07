a8e068846a133affaedc0c77b083400921c2ea62
amolikvivian <amolikvivian@gmail.com>
2022-05-08 04:13:19 +0530

    Re-routing root to 'cards/all'

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,15 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: () => import("../views/Home.vue"),
-  },
-  {
     path: '/cards/:id',
     name: "Cards",
     component: () => import("../views/Cards.vue")
+    
   },
   {
     path: '/:pathMatch(.*)*',
-    component: () => import("../views/NotFound.vue")
+    component: () => import("../views/NotFound.vue"),
+    alias: ['/']
   }
 ];
 
