655a3306f8bc8f3fc8e4e9714937f9f89e7909f8
amolikvivian <amolikvivian@gmail.com>
2022-05-08 14:24:46 +0530

    Bug fix: Infinite scroll

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -59,9 +59,9 @@ export default {
   },
   watch: {
     tab() {
-      if (this.tab === "owned") {
+      if (this.tab === "owned")
         this.$store.dispatch("getOwnerData", this.currentId);
-      }
+
       this.loading = true;
       this.loadFlag = true;
       this.list = [];
@@ -78,6 +78,7 @@ export default {
         let fetched = [];
         if (this.tab === "all") {
           fetched = await getAllData(this.page, this.limit);
+          console.log(fetched);
         } else if (this.tab === "owned") {
           fetched = await getDataByOwnerId(
             this.currentId,
@@ -95,10 +96,12 @@ export default {
     async search(query) {
       if (query.length === 0) {
         this.loadFlag = true;
+        this.page = 2;
         let fetched = [];
         if (this.tab == "owned") {
           fetched = await getDataByOwnerId(this.currentId, 1, this.limit);
-        } else if (this.tab == "all") {
+        }
+        if (this.tab == "all") {
           fetched = await getAllData(1, this.limit);
         }
         this.loading = false;
@@ -139,12 +142,13 @@ export default {
       }
     },
     async clear() {
+      this.loadFlag = true;
       let fetched = [];
+      this.page = 2;
       if (this.tab === "owned")
         fetched = await getDataByOwnerId(this.currentId, 1, this.limit);
 
       if (this.tab === "all") fetched = await getAllData(1, this.limit);
-
       this.list = [...fetched];
     },
   },
